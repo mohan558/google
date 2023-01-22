275cf1e63ffbc1d7536ae6ab69c577641340d7fb
mohan558 <gkannan741@gmail.com>
2023-01-22 23:01:54 +0530

    Hello

---
+++ src/test/java/org/test/git/GitFullClass.java
@@ -0,0 +1,16 @@
+package org.test.git;
+
+public class GitFullClass {
+	private void tc1() {
+		System.out.println("gokul");
+	}
+	private void tc2() {
+		System.out.println("Thamu");
+	}
+	private void tc3() {
+		System.out.println("Siva");
+	}
+	private void tc4() {
+		System.out.println("Murali");
+	}
+}
